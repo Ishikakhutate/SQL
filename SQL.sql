9d978abd12f79c4d77adfbbf5789397c05a36444
Ishikakhutate <154323837+Ishikakhutate@users.noreply.github.com>
2024-02-08 00:17:05 +0530

    Add files via upload

--- SQL.sql
+++ SQL.sql
@@ -1,70 +1,31 @@
-show databases;
+use classicmodels;
 
-create database cds16;
-create database sample;
-drop database sample;
-show tables;
+select*from employees;
+select employeeNumber,lastname,firstname,jobTitle from employees; 
 
-use cds16;
+select distinct jobTitle from employees;
 
-CREATE TABLE employee_details(
-emp_id VARCHAR(8),
-emp_name VARCHAR(20),
-emp_designation VARCHAR(20),
-emp_age INT);
+select employeeNumber,firstname,lastname,officeCode
+from employees
+Where officecode=1;
 
-create table Student_details(
-id varchar(10),
-Stu_name varchar(20),
-contact varchar(20),
-address varchar(20),
-Stu_age int);
-show tables;
-drop table Student_details;
+select employeeNumber,firstname,lastname,officeCode,jobtitle
+from employees
+where jobtitle = 'VP Marketing' and officeCode = 1;
 
-Insert into employee_details VALUES
-('1101', 'Ishika','Data scientist', 22);
-show tables;
+select employeeNumber,firstname,lastname,officeCode,jobtitle
+from employees
+where jobtitle = 'VP Marketing' or officeCode = 1;
 
-select*from employee_details;
-Insert into employee_details VALUES
-('1101', 'Ishika','Data scientist', 22),
-('1102', 'Mihir', 'Student', 20),
-('1103', 'Sudarshan','Topper', 22);
+select employeeNumber,firstname,lastname,officeCode
+from employees 
+order by officecode desc;
 
-update employee_details 
-SET emp_age = 23 
-WHERE emp_id='1102';
+select*from employees
+where jobtitle is null;
 
-update employee_details 
-set emp_age = 10 , emp_name = 'Isha'
-where emp_id ='1103';
-select*from employee_details;
-
-delete from employee_details where emp_id = '1101';
-describe employee_details;
-show columns from employee_details;
-
-alter table employee_details ADD city_name varchar(20);
-describe employee_details;
-
-alter table employee_details Drop column city_name;
-
-alter table employee_details modify emp_designation varchar(50);
-
-alter table employee_details rename column  emp_age to age;
-
-alter table employee_details 
-rename to employee; 
-
-select* from employee;
-
-Truncate table employee;
-
-
-
-
-
+select*from employees
+limit 3;
 
 
 
